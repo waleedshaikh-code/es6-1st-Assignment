7cefeacb50b1a7c7039b685ac8b36a00a7fca862
waleed shaikh <waleedshykh5@gmail.com>
2021-03-23 23:56:53 +0500

    WORK COMPLETE

---
+++ app.js
@@ -0,0 +1,30 @@
+//ASSIGNMENT - 1
+
+//Destructuring of nested array
+
+// const student = ["csharp", "phython", "php", "mongodb", "Java", ["1", "2", "3", "4", "5"]]
+// const [csharp, phython, php, mongodb, Java, [one, two, three, four, five]] = student;
+
+// console.log(one, phython, two, php);
+
+//Extract keys from object
+
+// const student = {
+//     name: 'waleed',
+//     profession: 'Full Stack developer',
+//     experience:'5 year',
+
+//   };
+//  console.log(Object.keys(student));
+
+
+//Extract values from object
+
+// const student = {
+//     name: 'waleed',
+//     profession: 'Full Stack developer',
+//     experience: '5 year',
+
+// };
+
+//   console.log(Object.values(student));
